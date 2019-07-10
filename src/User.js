68031ee73c8b46dc693cc2d7e851d0e03ecb2464
bmanek <41875460+bmanek@users.noreply.github.com>
2019-07-10 11:34:48 -0400

    add lifetime error reporting to User component

--- src/User.js
+++ src/User.js
@@ -4,16 +4,57 @@ import GameSettings from './GameSettings'
 
 export default class User extends Component {
 
+// Only tracks number of questions wrong per note. Should be expanded.
+
+constructor(){
+  super()
+  this.state = {
+    errors: ""
+  }
+}
+
 getStats = () => {
   fetch("http://localhost:3000/api/v1/sessions")
   .then(res => res.json())
   .then(data => {
-    data.map(error => {
-      console.log(error.type_wrong)
+    this.displayErrors(this.countErrors(data))
     })
-  })
-}
+  }
 
+countErrors = (data) => {
+  let A = 0
+  let B = 0
+  let C = 0
+  let D = 0
+  let E = 0
+  let F = 0
+  let G = 0
+  data.map(tone => {
+    if (tone.type_wrong === "A") {
+      A += 1 }
+    if (tone.type_wrong === "B") {
+      B += 1 }
+    if (tone.type_wrong === "C") {
+      C += 1 }
+    if (tone.type_wrong === "D") {
+      D += 1 }
+    if (tone.type_wrong === "E") {
+      E += 1 }
+    if (tone.type_wrong === "F") {
+      F += 1 }
+    if (tone.type_wrong === "G") {
+      G += 1 }
+    })
+     return(
+      `A: ${A}, B: ${B}, C: ${C}, D: ${D}, E: ${E}, F: ${F}, G: ${G}`
+   )
+ }
+
+ displayErrors = (errors) => {
+   this.setState({
+     errors: errors
+   })
+ }
 
 // incomplete method above. Still unsure how to render directly to page.
 // Previously it just console.logged "Settings, bruh"
@@ -21,10 +62,16 @@ getStats = () => {
   render() {
     return(
       <React.Fragment>
-        <h1>THIS IS USER COMPONENT</h1>
+        <h1>Username</h1>
+        <h2>You've played 14 games. Would you like to start a new one?</h2>
+        <p>Click 'Report' to view your lifetime stats!</p>
         <button onClick={this.getStats}>Report</button>
         <div className="report">
-          <p>This is the report div</p>
+        <br>
+        </br>
+          {(this.state.errors !== "") ?
+          this.state.errors.split(",").map(tone => {
+            return(<li>{tone} errors</li>)}) : null }
         </div>
         <Router>
           <Route exact path="/game-settings" component={GameSettings} />
