d163d06a60eac9f6fc1eb7b264cde6cc97c410bb
bmanek <41875460+bmanek@users.noreply.github.com>
2019-09-02 20:57:05 -0400

    add stub Login component

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './User'
+import './App.css';
+
+function App() {
+  return (
+    <div className="Login">
+      <User />
+    </div>
+  );
+}
+
+export default App
